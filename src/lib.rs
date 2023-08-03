c04c5120b1bfb035f4e5ce3453e43bd012554325
naturecodevoid <44983869+naturecodevoid@users.noreply.github.com>
2023-08-03 00:03:20 -0700

    1.0.0 (and publish to crates.io)

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,7 @@ Provides `Failure(String)` implementing `std::error::Error`. Includes convenienc
 **This crate should not be used in libraries.** Instead, use something like [`thiserror`](https://docs.rs/thiserror). For libraries, it is much better to have specific errors so library users can
 handle them better.
 
-This crate is not yet on crates.io because I haven't published any crates that use it to crates.io yet. If you use this crate and need to publish your crate to crates.io, create an issue or contact me
-via other means.
+[Documentation](https://docs.rs/cli-failure) | [crates.io](https://crates.io/crates/cli-failure)
 
 ## Example
 
